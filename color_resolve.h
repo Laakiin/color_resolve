91f2d96adb2eacf7b2ee5060ceaa9b374036df63
Anakin Mrq <76254305+Laakiin@users.noreply.github.com>
2023-11-15 12:27:24 +0100

    Update color_resolve.h

--- color_resolve.h
+++ color_resolve.h
@@ -15,4 +15,22 @@
 
 #include "stb_image.h"
 
+typedef struct{
+	unsigned char r;
+	unsigned char g;
+	unsigned char b;
+}pixel_t;
+
+typedef struct{
+	int height;
+	int width;
+	int channels;
+	pixel_t** array;
+}image_t;
+
+//get height width and number of channels of an image, store it in img
+int getImage(image_t* img,char* image_dir);
+//get image pixels, stored in pixel_t 2D array
+int getImageData(image_t* img, unsigned char* data);
+
 #endif /* COLOR_RESOLVE_H_ */
